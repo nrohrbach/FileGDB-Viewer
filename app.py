759c03fb6c262dbd155a0f6f688d9d62ea0a7774
nrohrbach <52918447+nrohrbach@users.noreply.github.com>
2025-07-29 16:31:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ if zip_data:
                                 centroid = df.geometry.centroid.dropna()
                                 m = folium.Map(
                                     location=[centroid.y.mean(), centroid.x.mean()],
-                                    zoom_start=6,
+                                    zoom_start=8,
                                     tiles="https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg",
                                     attr='Map data: &copy; <a href="https://www.swisstopo.ch" target="_blank" rel="noopener noreferrer">swisstopo</a>;<a href="https://www.bafu.admin.ch/" target="_blank" rel="noopener noreferrer">BAFU</a>'
                                 )
