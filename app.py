9e40292bfbca5af83aead748e8e573238cdbf3b5
nrohrbach <52918447+nrohrbach@users.noreply.github.com>
2025-07-29 16:08:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -74,13 +74,17 @@ if zip_data:
 
                             if not gdf.empty and gdf.geometry.notnull().any():
                                 centroid = gdf.geometry.centroid.dropna()
+                                                                
+                                # Nur Geometrie und ggf. ausgewählte Spalten für die Karte verwenden
+                                geojson_data = gdf[['geometry']].to_json()
+
                                 m = folium.Map(
                                     location=[centroid.y.mean(), centroid.x.mean()],
                                     zoom_start=8,
                                     tiles="https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg",
                                     attr='Map data: &copy; <a href="https://www.swisstopo.ch" target="_blank" rel="noopener noreferrer">swisstopo</a>;<a href="https://www.bafu.admin.ch/" target="_blank" rel="noopener noreferrer">BAFU</a>'
                                 )
-                                folium.GeoJson(gdf).add_to(m)
+                                folium.GeoJson(geojson_data, name=layer).add_to(m)
                                 st_folium(m, width=1000, height=600)
                             else:
                                 st.warning("Keine gültige Geometrie zum Anzeigen gefunden.")
